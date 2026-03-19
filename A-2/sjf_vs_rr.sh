1dcb7a8cc450fa7341cd431d28bddf8cca32e2c7
Sameer <sameerapatil.2605@gmail.com>
2026-03-19 16:01:50 +0530

    report edited

--- A-2/sjf_vs_rr.sh
+++ A-2/sjf_vs_rr.sh
@@ -8,7 +8,7 @@ for users in $(seq 5 10 8000)
 do
     echo "Running simulation with users=$users" >> ./SJF_vs_RR/RR_out.log
     
-    ./simulator/websim --users $users --simtime 180000 --warmup 20000 --trace_on 0 --num_cores 4 --max_threads 256 --thread_queue_limit 15000 --quantum 300 --think_base 3000 --context_switch_overhead 0.2 --think_mean_exp 200 --retry_limit 0\
+    ./simulator/websim --users $users --simtime 180000 --warmup 20000 --trace_on 0 --num_cores 4 --max_threads 256 --thread_queue_limit 15000 --quantum 10 --think_base 3000 --context_switch_overhead 0.2 --think_mean_exp 200 --retry_limit 0\
     --service_time_avg 60 --service_time_dist exponential --timeout_lower 20000 --timeout_upper 30000 --rng_seed $users --sched_policy RR >> ./SJF_vs_RR/RR_out.log 2>&1
     
     echo "--------------------------------------" >> ./SJF_vs_RR/RR_out.log
